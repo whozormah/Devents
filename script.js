727789230983d360009d09e795891d53d21f6b74
uzoma <uzoma.udoma@gmail.com>
2023-02-18 20:05:07 +0100

    Update form

--- script.js
+++ script.js
@@ -21,11 +21,11 @@
  * add event listener on multiple elements
  */
 
-// const addEventOnElements = function (elements, eventType, callback) {
-//   for (let i = 0, len = elements.length; i < len; i++) {
-//     elements[i].addEventListener(eventType, callback);
-//   }
-// }
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
 
 
 
